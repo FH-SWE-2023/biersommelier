42239e817b9fe2bae14b528c1c90485fb0b15d36
Simon Conrad <simon.conrad@alumni.fh-aachen.de>
2023-11-25 11:01:00 +0100

    feat: add delete image and pick image methods to the image manager

--- lib/imagemanager/ImageManager.dart
+++ lib/imagemanager/ImageManager.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:uuid/uuid.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageManager {
   /// Returns the local path of the app
@@ -20,11 +21,30 @@ class ImageManager {
   Future<String> saveImage(File image) async {
     final path = await _localPath;
     const uuid = Uuid();
-    String key = uuid.v1();
+    String key = uuid.v4();
     await FlutterImageCompress.compressAndGetFile(
       image.absolute.path,
       '$path/$key.jpg',
     );
     return key;
+  }
+
+  /// Deletes the image with the given [key]
+  /// Returns a Future<FileSystemEntity> that completes with this FileSystemEntity when the deletion is done.
+  /// If the FileSystemEntity cannot be deleted, the future completes with an exception.
+  Future<FileSystemEntity> deleteImage(String key) async {
+    final path = await _localPath;
+    return File('$path/$key.jpg').delete();
+  }
+
+  /// Opens the image picker and returns the picked image
+  Future<File> pickImage() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      return File(image.path);
+    } else {
+      throw Exception('No image selected');
+    }
   }
 }
