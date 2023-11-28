75f29644e7f34a0692818e1bf7dc5f5b9db3d7c4
Moritz <moritz@voulter.com>
2023-11-28 11:18:25 +0100

    feat(Header): Add HeaderIcon enum

--- lib/components/Header.dart
+++ lib/components/Header.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
 
+enum HeaderIcon { back, add, none }
+
 ///Custom App Bar with Custom Title[title] and Custom Background Color[backgroundColor].
 ///
 ///To show the Back-Arrow the bool [showBackArrow] can be set
 ///To show the Add-Button the bool [showAdd] can be set
-class Header extends StatelessWidget implements 
-PreferredSizeWidget {
+class Header extends StatelessWidget implements PreferredSizeWidget {
   final String title;
   final Color backgroundColor;
-  final bool showBackArrow;
-  final bool showAdd;
+  final HeaderIcon icon;
 
   const Header({
-    super.key, 
-    required this.title, 
+    super.key,
+    required this.title,
     required this.backgroundColor,
-    required this.showBackArrow,
-    required this.showAdd,
+    required this.icon,
   });
 
   @override
@@ -25,30 +24,34 @@ PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Text(title,
-      style: const TextStyle(color: Colors.black, fontSize: 29.0, fontWeight: FontWeight.bold),
+      title: Text(
+        title,
+        style: const TextStyle(
+            color: Colors.black, fontSize: 29.0, fontWeight: FontWeight.bold),
       ),
       centerTitle: true,
-      actions: showAdd
-      ?
-      <Widget>[
-        IconButton(
-          icon: const Icon(Icons.add, size: 33.0),
-          onPressed: () {},
-          color: Colors.black,
-        )
-      ]
-      : null, 
-      leading: showBackArrow
-        ? IconButton(
-          icon: const Icon(Icons.chevron_left_rounded, size: 45.0,),
-           onPressed: () {
-            //Navigator.pop(context);
-            //context.jump(RutPage.home);
-          },
-          color: Colors.black,
-        )
-      : null,
+      actions: icon == HeaderIcon.add
+          ? <Widget>[
+              IconButton(
+                icon: const Icon(Icons.add, size: 33.0),
+                onPressed: () {},
+                color: Colors.black,
+              )
+            ]
+          : null,
+      leading: icon == HeaderIcon.back
+          ? IconButton(
+              icon: const Icon(
+                Icons.chevron_left_rounded,
+                size: 45.0,
+              ),
+              onPressed: () {
+                //Navigator.pop(context);
+                //context.jump(RutPage.home);
+              },
+              color: Colors.black,
+            )
+          : null,
       toolbarHeight: 75,
       backgroundColor: backgroundColor,
     );
