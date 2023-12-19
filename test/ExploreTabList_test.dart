68fc1566347b58f5723ad8f7ac342c6e8d0f495f
Daniel Kempf <daniel.kempf@fluidon.com>
2023-12-19 17:29:46 +0100

    feat(Test): Adding some tests v1

---
+++ test/ExploreTabList_test.dart
@@ -0,0 +1,24 @@
+import 'package:biersommelier/components/ExploreTabList.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('ExploreBar Widget Test', () {
+/*     testWidgets('Renders with default values', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: ExploreBar(),
+        ),
+      );
+
+      // Verify that the ExploreTabBar is rendered.
+      expect(find.byType(ExploreTabBar), findsOneWidget);
+
+      // Verify that the default ExploreList for Bars is rendered.
+      expect(find.byKey(const ValueKey('bar-0')), findsOneWidget);
+
+    }); */
+
+  });
+}
