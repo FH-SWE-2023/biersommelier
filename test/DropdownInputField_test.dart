a33d780180c78cb63380ae8f481117977ff08b51
Daniel Kempf <daniel.kempf@fluidon.com>
2023-12-19 18:18:28 +0100

    feat(Test): add test with filters

--- test/DropdownInputField_test.dart
+++ test/DropdownInputField_test.dart
@@ -39,4 +39,43 @@ void main() {
     await tester.pump();
     expect(selectedOption, 'Option1');
   });
+
+
+  testWidgets('DropdownInputField displays options correctly with filter', (WidgetTester tester) async {
+    String? selectedOption;
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: DropdownInputField(
+            labelText: 'Select Option',
+            optionsList: [
+              TestDropdownOption(name: 'Option1', icon: 'delete.png', address: 'Address1'),
+              TestDropdownOption(name: 'Option2', icon: 'delete.png', address: 'Address2')
+            ],
+            onOptionSelected: (option) {
+              selectedOption = option.name;
+            },
+          ),
+        ),
+      ),
+    );
+
+    // Tap on the TextFormField to open the dropdown
+    await tester.tap(find.byType(TextFormField));
+    await tester.pump();
+
+    expect(find.text('Option1 '), findsOneWidget);
+    expect(find.text('Option2 '), findsOneWidget);
+
+    await tester.enterText(find.byType(TextFormField), 'Option1');
+    await tester.pump();
+    expect(find.text('Option1 '), findsOneWidget);
+    expect(find.text('Option2 '), findsNothing);
+
+    await tester.tap(find.text('Option1 '));
+    await tester.pump();
+    expect(selectedOption, 'Option1');
+  });
+
 }
